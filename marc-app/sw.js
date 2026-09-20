be78b606a6a6d0f2a22b83dc6a52e68a4942ad91
Naomi <naomigrindlay@gmail.com>
2026-09-20 14:41:20 -0600

    Bust offline cache for planner update

--- marc-app/sw.js
+++ marc-app/sw.js
@@ -1,1 +1,2 @@
-const C="marcs-renovations-v2",A=["./","./index.html","./manifest.json"];self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!=="marcs-renovations-v4").map(k=>caches.delete(k))))));
+const C="marcs-renovations-v4",A=["./","./index.html","./manifest.json"];self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
